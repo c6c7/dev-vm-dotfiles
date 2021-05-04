8300d12505932c4bcba0ccbfef22fa02979ceb87
Charles Celerier <dev@chckyn.org>
2021-05-04 02:31:40 -0700

    Initial commit of dotfiles

---
+++ config/guix/channels.scm
@@ -0,0 +1,10 @@
+(cons* (channel
+        (name 'chckyn)
+        (url "https://github.com/chckyn/guix-channel.git")
+        (branch "main")
+        (introduction
+         (make-channel-introduction
+          "daa642fbaa53e9b6f1cfd22ea013e5fdf4e17e3a"
+          (openpgp-fingerprint
+           "2116 4C25 323C EAF7 611E  49E6 9CFB 4F28 C311 D53D"))))
+       %default-channels)
