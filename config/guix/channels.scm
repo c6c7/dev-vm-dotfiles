a5c262b3d54733c3f5877ae321df08846d4233b4
Charles Celerier <charles@chckyn.org>
2021-06-14 23:43:18 -0700

    Add nonguix channel

--- config/guix/channels.scm
+++ config/guix/channels.scm
@@ -15,4 +15,13 @@
           "daa642fbaa53e9b6f1cfd22ea013e5fdf4e17e3a"
           (openpgp-fingerprint
            "2116 4C25 323C EAF7 611E  49E6 9CFB 4F28 C311 D53D"))))
+       (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        ;; Enable signature verification:
+        (introduction
+         (make-channel-introduction
+          "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+          (openpgp-fingerprint
+           "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
        %default-channels)
